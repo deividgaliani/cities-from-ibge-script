21a18a9f9ce184f49e47a0f8b2b3e83f3ea1ec02
deividgaliani <deividgaliani@gmail.com>
2023-03-15 22:49:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const { Pool, Client } = require("pg");
+const axios = require("axios");
+
+const client = new Client({
+  user: "pet",
+  host: "localhost",
+  database: "petDB",
+  password: "x12345",
+  port: "5432",
+});
+
+// Retrieve all Brazilian cities from the IBGE API
+async function retrieveCities() {
+  const url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios";
+
+  try {
+    const { data } = await axios.get(url);
+    console.log(`Found ${data.length} cities`);
+    return data.map((city) => ({
+      name: city.nome.replace("'", " "),
+      state: city.microrregiao.mesorregiao.UF.sigla,
+      ibge_code: city.id,
+    }));
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+// Insert cities on database
+async function insertCities(cities) {
+  client
+    .connect()
+    .then(() => console.log("connected"))
+    .catch((err) => console.error("connection error", err.stack));
+
+  cities.forEach(async (city) => {
+    try {
+      await client.query(
+        `INSERT INTO cities (name, state, ibge_code, created_at, updated_at) VALUES ('${city.name}', '${city.state}', ${city.ibge_code}, NOW(), NOW())`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  await client.query("COMMIT");
+  await client.release;
+}
+
+// Main program flow
+async function main() {
+  const cities = await retrieveCities();
+  if (cities.length > 0) {
+    await insertCities(cities);
+    console.log("Cities entered successfully");
+    process.exit(1);
+  } else {
+    console.error("No cities retrieved");
+    process.exit(1);
+  }
+}
+
+main();
